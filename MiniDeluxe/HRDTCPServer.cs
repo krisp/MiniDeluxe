cae69552c0513085620484a981a0a94e91e2db31
krisp <krisp@krisp.com>
2010-03-03 08:32:12 -0500

    added file

---
+++ MiniDeluxe/HRDTCPServer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections;
+
+namespace MiniDeluxe
+{
+    class HRDTCPServer
+    {
+        private bool stopListening = false;
+        private ArrayList clients;
+
+        public HRDTCPServer()
+        {
+            Thread ListenerThread = new Thread(new ThreadStart(ListenerThread));
+            ListenerThread.Start();
+        }
+
+        private void ListenerThread()
+        {
+
+        }
+    }
+}
